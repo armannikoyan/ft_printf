b0a9bd1d91bb86af2c038fd8ce8c62dbae13d552
Arman Nikoyan <armannikoyan@proton.me>
2024-03-28 18:41:50 +0400

    Norm error resolvation. [Too many lines left]

--- src/ft_printf_int.c
+++ src/ft_printf_int.c
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 21:44:29 by anikoyan          #+#    #+#             */
-/*   Updated: 2024/03/28 18:16:48 by anikoyan         ###   ########.fr       */
+/*   Updated: 2024/03/28 18:40:52 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ static int	ft_padding_width(char *number, t_flags *flags)
 	}
 	if (flags->plus == 1 && number[0] != '-')
 		padding_width--;
-	// if (flags->space == 1 && padding_width <= 0 && number[0] != '-')
-	// 	padding_width = 1;
 	if (flags->zero == 1 && padding_width > 0 && (len == 0
 			|| flags->precision >= 0))
 	{
@@ -65,7 +63,9 @@ static char	*ft_process_flags(char *number, int *padding_width
 			return (NULL);
 		}
 	}
-	if (flags->space == 1 && number[0] != '-' && flags->plus == 0 && (flags->minus == -1 || (flags->minus == 0 && *padding_width <= 0) || flags->minus == 1))
+	if (flags->space == 1 && number[0] != '-' && flags->plus == 0
+		&& (flags->minus == -1 || (flags->minus == 0
+				&& *padding_width <= 0) || flags->minus == 1))
 	{
 		if (ft_putchar(' ', result) == -1)
 		{
